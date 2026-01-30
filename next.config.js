82a7986d528e5576c7a0b09c3aea4741184c5089
Oil <sudarat@1moby.com>
2026-01-30 19:13:57 +0700

    gh-pages

--- next.config.js
+++ next.config.js
@@ -1,13 +1,12 @@
-console.log("🔥 USING next.config.mjs");
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
+  reactStrictMode: true,
   output: "export",
-  basePath: "/my-app-ig",
   assetPrefix: "/my-app-ig/",
-  trailingSlash: true,
   images: {
     unoptimized: true,
   },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
